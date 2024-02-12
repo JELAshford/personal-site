c8042c6bf24ef869ecdc55fe18570042659233f8
James Ashford <james.ashford@bnc.ox.ac.uk>
2024-02-12 19:59:13 +0000

    Quartz sync: Feb 12, 2024, 7:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["#private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       cdnCaching: true,
